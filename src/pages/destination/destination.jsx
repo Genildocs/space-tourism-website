d9fc7152a99b70d4502e8f57355007a9388a9cd5
genildocs <genildocs@gmail.com>
2023-09-14 06:17:44 -0300

    commit

--- src/pages/destination/destination.jsx
+++ src/pages/destination/destination.jsx
@@ -7,18 +7,19 @@ import Moon from "./lunes/moon.jsx";
 import Europa from "./lunes/europa.jsx";
 import Mars from "./lunes/mars.jsx";
 import Titan from "./lunes/titan.jsx";
-import React from "react";
+import React, { useState } from "react";
 
 export default function Destination() {
+  const[visible, setVisible] = useState(false)
   return (
     <ContainerDestination className="bg-[url('../public/assets/destination/background-destination-mobile.jpg')] sm:bg-[url('../public/assets/destination/background-destination-tablet.jpg')] lg:bg-[url('../public/assets/destination/background-destination-desktop.jpg')]">
       <Header />
       <PagesTitle number={"01"} title={"Pick your destination"} />
       <div className="text-white">
-         <Link to="/destination/moon"><button>Moon</button></Link> 
-          <button>Mars</button>
-          <button>Europa</button>
-          <button>Titan</button>
+         <Link to="/destination/moon"><button >Moon</button></Link>         
+         <Link to="/destination/mars"><button>Mars</button></Link>
+         <Link to="/destination/europa"><button>Europa</button></Link>
+         <Link to="/destination/titan"><button>Titan</button></Link>
       </div>
     </ContainerDestination>
   );
